b0a38ed815464df6cfe4b946af7d4d2d1e5efc40
Jose Manuel Cortes Ceron <230110688@itsoeh.edu.mx>
2023-11-25 12:34:51 -0600

    Subi el ejercicio 6 con el readme, voy a cambiar cosas

--- Ejercicios/Problema 5/Problema 5.java
+++ Ejercicios/Problema 5/Problema 5.java
@@ -1,12 +1,13 @@
-package proyectointegrador;
+package Ejercicios;
 
 import java.util.Scanner;
 
 /**
  *
- * @author Josee
+ * @author Josee 
+ * Dado un numero binario de n bits regresar su equivalente en decimal.
  */
-public class Cinco {
+public class Problema_5 {
 
     /**
      * @param args the command line arguments
@@ -14,39 +15,47 @@ public class Cinco {
     public static void main(String[] args) {
 
         String numeroBin;
-
+        int resp;
         Scanner binario = new Scanner(System.in);
 
         do {
-            System.out.print("Ingresa un número binario: ");
-            numeroBin = binario.nextLine();
+            do {
+                System.out.print("Ingresa un número binario: ");
+                numeroBin = binario.nextLine();
 
-            if (numeroBin.isEmpty()) {
-                System.out.println("Por favor ingresa un numero binario");
-            }
-            if(numeroBin.contentEquals("2")){
-                System.out.println("Los numeros binarios solo aceptan 0 y 1");
-            }
+                if (numeroBin.isEmpty()) {
+                    System.out.println("Por favor ingresa un numero binario");
+                } else if (!esBinario(numeroBin)) {
+                    System.out.println("El número ingresado no es binario (solo 0 y 1).");
+                }
 
-        } while (numeroBin.isEmpty() || numeroBin.contentEquals("2"));
+            } while (numeroBin.isEmpty() || !esBinario(numeroBin));
 
-        int resultadoDecimal = badecim(numeroBin);
+            int resultadoDecimal = binarioADecimal(numeroBin);
+            System.out.println("El número binario " + numeroBin + " en decimal es: " + resultadoDecimal);
 
-        System.out.println("El número binario " + numeroBin + " es equivalente a " + resultadoDecimal + " en decimal.");
-
+            System.out.println("¿Deseas convertir otro? 1=SI 0=NO");
+            resp = binario.nextInt();
+        } while (resp == 1);
     }
 
-    public static int badecim(String binario) {
-        
+    // Verifica si el número ingresado es binario (contiene solo 0 y 1)
+    public static boolean esBinario(String numero) {
+
+        return numero.matches("[01]+");
+    }
+
+    // Convierte un número binario a decimal
+    public static int binarioADecimal(String binario) {
+
+        int decimal = 0;
         int longitud = binario.length();
-        int resultado = 0;
 
         for (int i = 0; i < longitud; i++) {
-            int digito = Character.getNumericValue(binario.charAt(i));
-            int potencia = longitud - i - 1;
-            resultado += digito * Math.pow(2, potencia);
+            int digito = Character.getNumericValue(binario.charAt(longitud - i - 1));
+            decimal += digito * Math.pow(2, i);
         }
 
-        return resultado;
+        return decimal;
     }
 }
